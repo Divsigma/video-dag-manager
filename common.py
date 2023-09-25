ffe8acdf6d1bc685b3d814cd04e17b71336df59a
divsigma <pontsh@163.com>
2023-09-25 16:38:50 +0800

    fix: upload common.py

---
+++ common.py
@@ -0,0 +1,7 @@
+SYNC_RESULT_KEY_APPEND = "appended_result"
+SYNC_RESULT_KEY_LATEST = "latest_result"
+SYNC_RESULT_KEY_PLAN = "ext_plan"
+SYNC_RESULT_KEY_RUNTIME = "ext_runtime"
+
+PLAN_KEY_VIDEO_CONF = "video_conf"
+PLAN_KEY_FLOW_MAPPING = "flow_mapping"
